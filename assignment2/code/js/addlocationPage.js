2053f34d01ed85e58c773e0c8cb5c4fa2a696f11
Bohan Lin <blin23@student.monash.edu>
2016-05-14 19:44:28 +1000

    Add key word input bar in the "addlocation.html"

--- assignment2/code/js/addlocationPage.js
+++ assignment2/code/js/addlocationPage.js
@@ -1,2 +1,11 @@
 // Code for the Add Location page.
 
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 8
+  });
+}
+ //async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBWlFVMpgT7DMlVPh8h7jPj7tp3R6bodhI&callback=initMap"
+
